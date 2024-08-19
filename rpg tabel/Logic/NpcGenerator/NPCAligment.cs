16e91fb2156939691d46d50d12dd9acccf270392
ucnjakob <72885773+jakob12dk@users.noreply.github.com>
2024-08-19 14:58:16 +0200

    studd

---
+++ rpg tabel/Logic/NpcGenerator/NPCAligment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg_tabel.Logic.NpcGenerator
+{
+    public enum NPCAlignment
+    {
+        LawfulGood,
+        NeutralGood,
+        ChaoticGood,
+        LawfulNeutral,
+        TrueNeutral,
+        ChaoticNeutral,
+        LawfulEvil,
+        NeutralEvil,
+        ChaoticEvil
+    }
+}
